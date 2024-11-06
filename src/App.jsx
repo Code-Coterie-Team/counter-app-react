cc3ac0b058f3c2c6ea64cad4ba1d904a2eb5fad0
Abolfazl <abtarahomii@gmail.com>
2024-11-06 10:13:48 +0330

    add btn

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-export default function App() {
+const App = () => {
   return (
-    <div className="text-3xl bg-blue-200 h-screen">
-      <div>
-        
+    <div className="bg-blue-200 h-screen flex justify-center items-start p-12">
+      <div className=" bg-white w-6/12 h-3/6 flex flex-col">
+        <div className="text-[#102A42] flex justify-center py-4 text-[50px] font-medium">first counter</div>
+        <div className="text-[#102A42] flex justify-center text-[50px] font-medium">0</div>
+
+        <div className="flex justify-center py-6 gap-24 ">
+           <button className="border-[3px] border-[#222222] rounded-md w-36 uppercase">decrease</button>
+           <button>reset</button>
+           <button>increase</button>
+        </div>
       </div>
     </div>
   )
 }
+
+export default App;
